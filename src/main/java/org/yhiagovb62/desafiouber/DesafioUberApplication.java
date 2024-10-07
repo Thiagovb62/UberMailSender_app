2317a24d5890519fbff4e3e5a568ac1bf14f7acb
thiago.bezerra <thiago.bezerra@dataprev.gov.br>
2024-10-07 18:49:15 -0300

    initializing project spring boot

---
+++ src/main/java/org/yhiagovb62/desafiouber/DesafioUberApplication.java
@@ -0,0 +1,13 @@
+package org.yhiagovb62.desafiouber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yhiagovb62/desafiouber/DesafioUberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yhiagovb62.desafiouber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
